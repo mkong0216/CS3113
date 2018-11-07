89497eaaee2a1e17e5716a60d04ec2c5bc11e20d
Mandy Kong <mk5506@nyu.edu>
2018-11-06 22:26:18 -0500

    added back supporting files that are actually important and will break xcode if not there, oops

---
+++ HW #4/NYUCodebase/Entity.cpp
@@ -0,0 +1,9 @@
+//
+//  Entity.cpp
+//  NYUCodebase
+//
+//  Created by Mandy Kong on 11/4/18.
+//  Copyright © 2018 Ivan Safrin. All rights reserved.
+//
+
+#include "Entity.hpp"
---
+++ HW #4/NYUCodebase/SpriteSheet.cpp
@@ -0,0 +1,9 @@
+//
+//  SpriteSheet.cpp
+//  NYUCodebase
+//
+//  Created by Mandy Kong on 11/4/18.
+//  Copyright © 2018 Ivan Safrin. All rights reserved.
+//
+
+#include "SpriteSheet.hpp"
